d139ca7f36b1c9b3d61bb92d7fc8ad577ab134ba
asilva111 <45497798+asilva111@users.noreply.github.com>
2019-05-10 17:04:23 -0600

    Add files via upload

--- drawMaze.py
+++ drawMaze.py
@@ -1,22 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 24 16:17:23 2019
-
-@author: andre
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  8 22:29:30 2019
-
-@author: andre
-"""
-
-# Starting point for program to build and draw a maze
-# Modify program using disjoint set forest to ensure there is exactly one
-# simple path joiniung any two cells
-# Programmed by Olac Fuentes
-# Last modified March 28, 2019
+'''
+Created on Thu April 25 15:16:21 2019
+CS 2302 - Andres Silva
+> Teacher: Olac Fuentes
+> TAs: Anindita Nath  & Maliheh Zargaran
+> Lab #7
+> Create a maze of nxn size and plot a solution using graph implementations.
+> LAST MODIFIED: APRIL 28th, 2019
+'''
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,8 +14,24 @@ import random
 from dsf import *
 import time
 
-def draw_maze(walls,maze_rows,maze_cols,cell_nums=False):
+def draw_maze(EL,walls,maze_rows,maze_cols,cell_nums=False):
     fig, ax = plt.subplots()
+    
+    #Plot line from edge list 'EL'
+    for l in EL:
+        if l[1]-l[0] != 1: #vertical wall
+            lx0 = (l[1]%maze_cols) +.5
+            lx1 = lx0
+            ly0 = (l[1]//maze_cols) -.5
+            ly1 = ly0+1
+        else:#horizontal wall
+            lx0 = (l[0]%maze_cols) +.5
+            lx1 = lx0+1
+            ly0 = (l[1]//maze_cols) +.5
+            ly1 = ly0  
+        ax.plot([lx0,lx1],[ly0,ly1],linewidth=1,color='r') 
+    
+    
     for w in walls:
         if w[1]-w[0] ==1: #vertical wall
             x0 = (w[1]%maze_cols)
@@ -45,10 +51,13 @@ def draw_maze(walls,maze_rows,maze_cols,cell_nums=False):
         for r in range(maze_rows):
             for c in range(maze_cols):
                 cell = c + r*maze_cols   
-                ax.text((c+.5),(r+.5), str(cell), size=10,
+                ax.text((c+.5),(r+.5), str(cell), size = 10,
                         ha="center", va="center")
     ax.axis('off') 
     ax.set_aspect(1.0)
+    
+
+
 
 def wall_list(maze_rows, maze_cols):
     # Creates a list with all the walls in the maze
@@ -60,37 +69,28 @@ def wall_list(maze_rows, maze_cols):
                 w.append([cell,cell+1]) # wall between adjacent columns
             if r!=maze_rows-1: #if not last row
                 w.append([cell,cell+maze_cols]) # wall between adjacent rows
-    return w
 
+    return w
 
 
 def Maze_normal(r,c,S,W,m):
     i = 0
     EL = []
-    while i < m:
+    while i < m and i != (r*c)-1:
         d = random.randint(0,len(W)-1) #random index
         if find(S,W[d][0]) != find(S,W[d][1]): #If the roots are different,
             union(S,W[d][0],W[d][1]) #Join the sets,
             EL.append(W.pop(d)) #Delete wall
             i += 1
     
-    if m < (r*c) - 1:
-        print(" A path from source to destination is not guaranteed to exist")
-    elif m == (r*c) - 1:
-        print("The is a unique path from source to destination")
-    else:
-        print("There is at least one path from source to destination")
-        
+    if i == (r*c)-1: #Handling the infinite loop, delete as many walls as possible, disregarding the DSF
+        while i < m or i <= len(W):
+            d = random.randint(0,len(W)-1) #random index
+            union(S,W[d][0],W[d][1]) #Join the sets,
+            EL.append(W.pop(d)) #Delete wall
+            i += 1
+      
     return EL
-
-
-def Maze_C(r,c,S,W):
-    while num_of_sets(S) > 1:
-        d = random.randint(0,len(W)-1) #random index
-        if find_c(S,W[d][0]) != find_c(S,W[d][1]): #Use path compression
-            union_by_size(S,W[d][0],W[d][1]) #and union by size
-            W.pop(d)
-
 
 
 def EdgeList_to_AdjList(EL, size):     
@@ -105,37 +105,136 @@ def EdgeList_to_AdjList(EL, size):
     return AL
 
 
-plt.close("all") 
+def BFS(AL,s):
+    prev = np.zeros(len(AL), dtype = int)-1 #Initialize the prev array
+    visited = [False] * len(AL) #Keep track of the visited nodes
+    Q = [] #Create Queue
+    
+    Q.append(AL[0][0]) #Append source to Queue and mark it as visited
+    visited[AL[0][0]] = True
+    
+    while Q:
+        if prev[-1] >= 0: #If last node visited, loop is finished
+            break
+        
+        s = Q.pop(0) #Get next element in Queue 
+        
+        for i in AL[s]: #For all adjacent vertex to current node,
+            if visited[i] == False: #Mark it as visited
+                visited[i] = True
+                prev[i] = s #Mark the prev array at the neightboor location as s
+                Q.append(i) #Enqueue the neightboor for future visit
+           
+    prev[0] = -1 #Make the source node the root.
+
+    return prev #Return prev array
+
+
+def DFS(AL,s):
+    prev = np.zeros(len(AL), dtype=int)-1 #Initialize prev array 
+    visited = [False] * len(AL)  #Initialize visited array
+    Stack = [] #Create stack
+    
+    Stack.append(AL[0][0]) #Push source node and mark it as visited
+    visited[AL[0][0]] = True
+    
+    while Stack:
+        if prev[-1] >= 0: # Break loop at last vertex
+            break
+        s = Stack.pop() #retrieve next node to be visited
+        for i in AL[s]:
+            if visited[i] == False: #When visiting the neightboor, mark it as true
+                visited[i] = True
+                prev[i] = s #Keep track in the prev array
+                Stack.append(i) #Push neightboor
+        
+        if Stack == []:
+            S.append(AL[0][1])
+    
+    return prev
+
+
+def prev_to_EL(prev):
+    EL = [] #Create edge list
+    
+    i = len(prev)-1 #Start from last node
+    while prev[i] >= 0: #While not reaching the end
+        if i < prev[i]: #Decide direction of line
+            edge = [i,prev[i]] #Create an edge form current index to prev[i]
+        else:
+            edge = [prev[i],i]
+        
+        i = prev[i] #Update index to content of array
+        EL.append(edge) #append to edge list
+    
+    EL.append([0,i]) #Finally, create a node with 0 to last known index
+    
+    return EL
+    
+
+
+"""
+MAIN __________________________________________________________________________
+"""
+def main():
+    
+    r = int(input("Please enter number of rows for the square maze\n"))
+    c = r
+    
+    print("There are ", r*c, " cells", " and ", (r*c*2)," walls.")
+    
+    m = int(input("Please enter walls to be removed\n"))
+        
+    if m < (r*c) - 1:
+        print("A path from source to destination is not guaranteed to exist, terminating program...")
+        return
+    
+    elif m == (r*c) - 1:
+        print("There is a unique path from source to destination")
+    
+    else:
+        print("There is at least one path from source to destination")
+
+    selection  = input("Please select method to find path:     BSF/DFS\n")
+    
+    
+
+    W = wall_list(r,c)
+    S = DisjointSetForest(r * c)   
+    
+    Edge_List = Maze_normal(r,c,S,W,m)
+    
+    AL = EdgeList_to_AdjList(Edge_List, r*c)
+    
+    if selection == "BSF":
+        start = time.time()
+        
+        prev = BFS(AL,0)
+        path = prev_to_EL(prev)
+        
+        end = time.time()
+        print(end - start)
+
+    
+    elif selection == "DFS":
+        start = time.time()
+        
+        prev = DFS(AL,0)
+        path = prev_to_EL(prev)
+        
+        end = time.time()
+        print(end - start)
+        
+    else:
+        print("Invalid input, exiting..")
+        return
+        
+    
+    draw_maze(path,W,r,c) 
 
 
 
-r = 2 #Define number of rows
-c = r #Define number of columns
-
-
-print("There are ", r*c, " cells" )
-m = int(input("Please enter m\n"))
-
-W = wall_list(r,c)
-S = DisjointSetForest(r * c)   
-
-
-
-
-start = time.time()
-
-Edge_List = Maze_normal(r,c,S,W,m)
-AL = EdgeList_to_AdjList(Edge_List, r*c)
-print(AL)
-draw_graph(AL)
-
-
-end = time.time()
-
-#print("Using using standard union and no compression - Time: ", end - start)
-
-
-draw_maze(W,r,c) 
+main()
 
 
 
@@ -147,11 +246,7 @@ draw_maze(W,r,c)
 
 
 
-
-
-
-
-
+#plt.close("all") 
 
 
 
